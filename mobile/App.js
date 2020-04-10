dc398190c367ce179d8873fc292914033ae6d757
Mauro Cesar <maurocesar@MacBook-Pro-de-Mauro.local>
2020-04-10 00:28:34 -0300

    Finalizacao do projeto, desenvolvimento mobile

--- mobile/App.js
+++ mobile/App.js
@@ -3,6 +3,9 @@ import { Text, View } from 'react-native';
 
 import Routes from './src/routes';
 
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
 export default function App() {
   return (
     <Routes />
